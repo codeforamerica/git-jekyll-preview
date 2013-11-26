03a1d16e206f5e186c9fd8cef10acf6670a6767d
Michal Migurski <mike@teczno.com>
2013-11-26 05:15:51 +0000

    Added correct redirects based on Referer headers

--- make-it-so.py
+++ make-it-so.py
@@ -1,35 +1,64 @@
 from logging import DEBUG, basicConfig
 from os.path import join, isdir, isfile
 
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 
 from git import prepare_git_checkout
+from href import needs_redirect, get_redirect
 from util import get_directory_response
 from util import get_file_response
 from jekyll import jekyll_build
 
 app = Flask(__name__)
 
+def should_redirect():
+    ''' Return True if the current flask.request should redirect.
+    '''
+    referer_url = request.headers.get('Referer')
+    
+    if not referer_url:
+        return False
+    
+    return needs_redirect(request.host, request.path, referer_url)
+
+def make_redirect():
+    ''' Return a flask.redirect for the current flask.request.
+    '''
+    referer_url = request.headers.get('Referer')
+    return redirect(get_redirect(request.path, referer_url), 302)
+
 @app.route('/')
 def hello_world():
+    if should_redirect():
+        return make_redirect()
+    
     return 'Hello world'
 
 @app.route('/<account>/<repo>')
 def repo_only(account, repo):
     ''' Redirect to "master" on a hunch.
     '''
+    if should_redirect():
+        return make_redirect()
+    
     return redirect('/%s/%s/master/' % (account, repo), 302)
 
 @app.route('/<account>/<repo>/')
 def repo_only_slash(account, repo):
     ''' Redirect to "master" on a hunch.
     '''
+    if should_redirect():
+        return make_redirect()
+    
     return redirect('/%s/%s/master/' % (account, repo), 302)
 
 @app.route('/<account>/<repo>/<ref>')
 def repo_ref(account, repo, ref):
     ''' Redirect to add trailing slash.
     '''
+    if should_redirect():
+        return make_redirect()
+    
     jekyll_build(prepare_git_checkout(account, repo, ref))
     return redirect('/%s/%s/%s/' % (account, repo, ref), 302)
 
@@ -37,6 +66,9 @@ def repo_ref(account, repo, ref):
 def repo_ref_slash(account, repo, ref):
     ''' Show repository root directory listing.
     '''
+    if should_redirect():
+        return make_redirect()
+    
     site_path = jekyll_build(prepare_git_checkout(account, repo, ref))
     return get_directory_response(site_path)
 
@@ -44,6 +76,9 @@ def repo_ref_slash(account, repo, ref):
 def repo_ref_path(account, repo, ref, path):
     ''' Show response for a path, whether a file or directory.
     '''
+    if should_redirect():
+        return make_redirect()
+
     site_path = jekyll_build(prepare_git_checkout(account, repo, ref))
     local_path = join(site_path, path)
     
@@ -57,6 +92,13 @@ def repo_ref_path(account, repo, ref, path):
         return get_directory_response(local_path)
     
     return 'finished path ' + path
+
+@app.route('/<path:path>')
+def all_other_paths(path):
+    '''
+    '''
+    if should_redirect():
+        return make_redirect()
 
 if __name__ == '__main__':
     basicConfig(level=DEBUG, format='%(levelname)06s: %(message)s')
