0de546cb867f764220cc09324e6a3de871a7155d
Michal Migurski <mike@teczno.com>
2013-11-26 05:34:07 +0000

    Preventing rapid Jekyll rebuilds with freshness check

--- jekyll.py
+++ jekyll.py
@@ -1,14 +1,18 @@
-from os.path import join
-from logging import debug
+from os.path import join, exists
+from logging import info
 
-from util import run_cmd
+from util import run_cmd, touch, is_fresh
 
 def jekyll_build(checkout_path):
     '''
     '''
     jekyll_path = join(checkout_path, '_site')
-    debug('Building jekyll ' + jekyll_path)
+
+    if exists(jekyll_path) and is_fresh(jekyll_path):
+        return jekyll_path
     
+    info('Building jekyll ' + jekyll_path)
     run_cmd(('jekyll', 'build'), checkout_path)
+    touch(jekyll_path)
     
     return jekyll_path
