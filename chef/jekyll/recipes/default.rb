2c774d0c992221654c198f22c70a3ca2c8523728
Michal Migurski <mike@teczno.com>
2013-12-01 19:22:32 +0000

    Fixed two chef gotchas

--- chef/jekyll/recipes/default.rb
+++ chef/jekyll/recipes/default.rb
@@ -1,10 +1,17 @@
 include_recipe 'ruby-2.0.0-p353'
 
-bash 'install jekyll' do
-    creates '/usr/local/bin/jekyll'
+bash 'install rdiscount' do
+    not_if 'gem list -i rdiscount'
     code '
     source /etc/profile.d/rvm.sh
     /usr/local/rvm/rubies/ruby-2.0.0-p353/bin/gem install rdiscount --no-ri --no-rdoc
+    '
+end
+
+bash 'install jekyll' do
+    not_if 'gem list -i jekyll'
+    code '
+    source /etc/profile.d/rvm.sh
     /usr/local/rvm/rubies/ruby-2.0.0-p353/bin/gem install jekyll --no-ri --no-rdoc
     '
 end
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
 #
 # Install the chef ruby gem if chef-solo is not in the path.
 # This script is safe to run multiple times.
@@ -9,6 +9,11 @@ if [ ! `which chef-solo` ]; then
 fi
 
 BASE=`dirname $0`
+PREFIX="${BASE:0:1}"
+
+if [ $PREFIX != '/' ]; then
+    BASE="`pwd`/${BASE}"
+fi
 
 chef-solo -c $BASE/chef/solo.rb \
           -j $BASE/chef/roles.json
