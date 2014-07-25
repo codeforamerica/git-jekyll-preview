c41904dbb66f8aef33c1d413b6df25ceba8d8cdc
Adam Becker <ad@mbecker.cc>
2014-07-24 22:04:33 -0400

    Use `github-pages` gem to manage ruby deps
    Closes #4

--- chef/jekyll/recipes/default.rb
+++ chef/jekyll/recipes/default.rb
@@ -1,17 +1,9 @@
 include_recipe 'ruby-2.0.0-p353'
 
-bash 'install rdiscount' do
-    not_if 'gem list -i rdiscount'
+bash 'install github-pages' do
+    not_if 'gem list -i github-pages'
     code '
     source /etc/profile.d/rvm.sh
-    /usr/local/rvm/rubies/ruby-2.0.0-p353/bin/gem install rdiscount --no-ri --no-rdoc
-    '
-end
-
-bash 'install jekyll' do
-    not_if 'gem list -i jekyll'
-    code '
-    source /etc/profile.d/rvm.sh
-    /usr/local/rvm/rubies/ruby-2.0.0-p353/bin/gem install jekyll --no-ri --no-rdoc
+    /usr/local/rvm/rubies/ruby-2.0.0-p353/bin/gem install github-pages --no-ri --no-rdoc
     '
 end
