d45fd90d63d2cadb8994d007e3a1a28652645d83
Michal Migurski <mike@teczno.com>
2014-09-30 19:46:57 -0700

    Added stubby SSL configuration to Apache resource

--- chef/apache/recipes/default.rb
+++ chef/apache/recipes/default.rb
@@ -10,6 +10,16 @@ link '/etc/apache2/mods-enabled/proxy.load' do
     action :create
 end
 
+link '/etc/apache2/mods-enabled/ssl.conf' do
+    to '/etc/apache2/mods-available/ssl.conf'
+    action :create
+end
+
+link '/etc/apache2/mods-enabled/ssl.load' do
+    to '/etc/apache2/mods-available/ssl.load'
+    action :create
+end
+
 link '/etc/apache2/mods-enabled/proxy_http.load' do
     to '/etc/apache2/mods-available/proxy_http.load'
     action :create
@@ -20,62 +30,7 @@ file '/etc/apache2/sites-available/jekit' do
     group 'root'
     mode '0755'
     action :create
-    content <<-conf
-<VirtualHost *:80>
-    ServerAdmin webmaster@localhost
-
-    DocumentRoot /var/www
-
-    <Directory />
-        Options FollowSymLinks
-        AllowOverride None
-    </Directory>
-
-    <Directory /var/www/>
-        Options Indexes FollowSymLinks MultiViews
-        AllowOverride None
-        Order allow,deny
-        allow from all
-    </Directory>
-
-    ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
-
-    <Directory "/usr/lib/cgi-bin">
-        AllowOverride None
-        Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
-        Order allow,deny
-        Allow from all
-    </Directory>
-
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-
-    # Possible values include: debug, info, notice, warn, error, crit,
-    # alert, emerg.
-    LogLevel warn
-
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-    Alias /doc/ "/usr/share/doc/"
-
-    <Directory "/usr/share/doc/">
-        Options Indexes MultiViews FollowSymLinks
-        AllowOverride None
-        Order deny,allow
-        Deny from all
-        Allow from 127.0.0.0/255.0.0.0 ::1/128
-    </Directory>
-
-    <Location />
-        ProxyPass http://127.0.0.1:8080/
-        ProxyPassReverse http://127.0.0.1:8080/
-    </Location>
-
-    <Proxy http://127.0.0.1:8080/*>
-        Allow from all
-    </Proxy>
-
-</VirtualHost>
-conf
+    content File.open(File.dirname(__FILE__) + "/apache-jekit.conf").read()
 end
 
 link '/etc/apache2/sites-enabled/000-default' do
@@ -84,6 +39,38 @@ end
 
 link '/etc/apache2/sites-enabled/jekit' do
     to '/etc/apache2/sites-available/jekit'
+    action :create
+end
+
+link '/etc/apache2/sites-enabled/default-ssl' do
+    to '/etc/apache2/sites-available/default-ssl'
+    action :delete
+end
+
+file '/etc/apache2/sites-available/jekit-ssl' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+    content File.open(File.dirname(__FILE__) + "/apache-ssl.conf").read()
+end
+
+file '/etc/ssl/certs/jekit_codeforamerica_org.crt' do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+end
+
+file '/etc/ssl/private/jekit-secret.key' do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+end
+
+link '/etc/apache2/sites-enabled/jekit-ssl' do
+    to '/etc/apache2/sites-available/jekit-ssl'
     action :create
 end
 
